2b10e573a1dc13d544743be70f04f3c8b9a20a9d
michal.novotny <michal.novotny@akademiasovy.sk>
2018-11-22 13:02:34 +0100

    GSD

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/michalnov/basicAPI/server"
 )
 
 func helloServer() {
 	fmt.Println("Hello server")
+}
+
+func main() {
+	var exit = make(chan int)
+	//var serv server.Server
+	err, serv := server.NewServer(":1201", exit)
+	if err != nil {
+		fmt.Println("Error while creating server")
+		return
+	} else {
+		go runServer(serv)
+		ex := <-exit
+		fmt.Println(ex)
+	}
+}
+
+func runServer(s server.Server) {
+
 }
